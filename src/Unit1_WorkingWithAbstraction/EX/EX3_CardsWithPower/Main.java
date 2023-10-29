b49d68ce4635c1b3208fcee31ef7b81648f004a1
Nikola Vasilev <nikola.vasilev99@icloud.com>
2023-10-29 11:56:31 +0200

    -

--- src/Unit1_WorkingWithAbstraction/EX/EX3_CardsWithPower/Main.java
+++ src/Unit1_WorkingWithAbstraction/EX/EX3_CardsWithPower/Main.java
@@ -6,6 +6,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner (System.in);
 
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Hello");
 
+        }
     }
 }
