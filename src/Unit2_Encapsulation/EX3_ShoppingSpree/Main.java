0203b88c4612bfd76ecdf461807526920811d36f
Nikola Vasilev <nikola.vasilev99@icloud.com>
2023-11-08 15:56:37 +0200

    -

---
+++ src/Unit2_Encapsulation/EX3_ShoppingSpree/Main.java
@@ -0,0 +1,9 @@
+package Unit2_Encapsulation.EX3_ShoppingSpree;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
